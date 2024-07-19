7f77cf26b2658aced1452ae115a1d5c100417833
Naman <156210246+Rishy-09@users.noreply.github.com>
2024-07-19 22:56:48 +0530

    Initial commit

---
+++ src/chatting/application/Client.java
@@ -0,0 +1,204 @@
+package chatting.application;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.net.*;
+import java.io.*;
+//server must be in running state then client will work
+public class Client implements ActionListener {
+    JTextField text;
+    static JPanel a1;
+    //    to show messages one below the other
+    static Box vertical = Box.createVerticalBox();
+    JButton send;
+    static DataOutputStream dout;
+    static JFrame f = new JFrame();
+    Client(){
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0,0 ,450, 70);
+        p1.setLayout(null);
+        f.add(p1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel back = new JLabel(i3);
+        back.setBounds(5, 20, 25, 25);
+        p1.add(back);
+
+//        mouse clicking on label
+        back.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/2.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50,50);
+        p1.add(profile);
+
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30,30);
+        p1.add(video);
+
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        JLabel phone = new JLabel(i12);
+        phone.setBounds(360, 20, 35,30);
+        p1.add(phone);
+
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+        JLabel morevert = new JLabel(i15);
+        morevert.setBounds(420, 20, 10,25);
+        p1.add(morevert);
+
+        JLabel name = new JLabel("Hmmm");
+        name.setBounds(110, 5, 100, 30);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        p1.add(name);
+
+        JLabel status = new JLabel("Active Now");
+        status.setBounds(110, 35, 100, 18);
+        status.setForeground(Color.WHITE);
+        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
+        p1.add(status);
+
+//        creating second panel
+        a1 = new JPanel();
+        a1.setBounds(5, 75, 440, 570);
+        f.add(a1);
+
+        text = new JTextField();
+        text.setBounds(5, 655, 310, 40);
+        text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(text);
+
+        send = new JButton("Send");
+        send.setBounds(320, 655, 123, 40);
+        send.setBackground(new Color(7, 94, 84));
+        send.setForeground(Color.WHITE);
+        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        send.addActionListener(this);
+        f.add(send);
+
+        f.setSize(450, 700);
+        f.setLocation(800, 50);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
+
+        f.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent ae){
+        try {
+            if (ae.getSource() == send) {
+                String out = text.getText();
+//            getting label via function to format the message
+                JPanel p2 = formatLabel(out);
+//            System.out.println(out);
+//            using border layout to put layouts on top, bottom, left, right, centre
+                a1.setLayout(new BorderLayout());
+//      setting it to right side
+                JPanel right = new JPanel(new BorderLayout());
+//            its argument doesn't accept string only panel so setting text into layout and that into panel
+                right.add(p2, BorderLayout.LINE_END);
+//            now putting that into box and to right hand side
+                vertical.add(right);
+//            space between two vertical struct
+                vertical.add(Box.createVerticalStrut(15));
+//            now adding this into a1 panel
+                a1.add(vertical, BorderLayout.PAGE_START);
+//            now reloading the frame to show the message while the program is running
+
+                dout.writeUTF(out);
+
+                text.setText("");
+                f.repaint();
+                f.invalidate();
+                f.validate();
+
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    public static JPanel formatLabel(String out){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel output = new JLabel("<html><p style=\"width: 150px\">"+ out + "</p></html>");
+        output.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        output.setBackground(new Color(37, 211, 102));
+        output.setOpaque(true);
+//        creating padding, filling border with emptiness for message
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        panel.add(output);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        JLabel time = new JLabel();
+        time.setText(sdf.format(cal.getTime()));
+
+        panel.add(time);
+
+        return panel;
+    }
+
+    public static void main(String[] args) {
+        new Client();
+
+        try{
+//            making socket
+//            as out host is local
+            Socket s = new Socket("127.0.0.1", 6001);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+//                sending messages out
+            dout = new DataOutputStream(s.getOutputStream());
+            while(true){
+                a1.setLayout(new BorderLayout());
+                String msg = din.readUTF(); // protocol
+//                    displaying the message
+                JPanel panel = formatLabel(msg);
+
+//                    to show the message to left side
+                JPanel left = new JPanel(new BorderLayout());
+                left.add(panel, BorderLayout.LINE_START);
+//                    calling non-static functions like vertical, validate from static function main so showing error
+//                    so converting the nature to static
+                vertical.add(left);
+//                    so we have to create object of the JFrame class ,so we can't extend this directly
+                vertical.add(Box.createVerticalStrut(15));
+                a1.add(vertical, BorderLayout.PAGE_START);
+                f.validate();
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/chatting/application/Server.java
@@ -0,0 +1,207 @@
+package chatting.application;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+//we have two classes server-socket and socket to make classes server and client respectively
+public class Server implements ActionListener {
+    JTextField text;
+    JPanel a1;
+//    to show messages one below the other
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+    JButton send;
+    static DataOutputStream dout;
+    Server(){
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0,0 ,450, 70);
+        p1.setLayout(null);
+        f.add(p1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel back = new JLabel(i3);
+        back.setBounds(5, 20, 25, 25);
+        p1.add(back);
+
+//        mouse clicking on label
+        back.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50,50);
+        p1.add(profile);
+
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30,30);
+        p1.add(video);
+
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        JLabel phone = new JLabel(i12);
+        phone.setBounds(360, 20, 35,30);
+        p1.add(phone);
+
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+        JLabel morevert = new JLabel(i15);
+        morevert.setBounds(420, 20, 10,25);
+        p1.add(morevert);
+
+        JLabel name = new JLabel("Nigga");
+        name.setBounds(110, 5, 100, 30);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        p1.add(name);
+
+        JLabel status = new JLabel("Active Now");
+        status.setBounds(110, 35, 100, 18);
+        status.setForeground(Color.WHITE);
+        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
+        p1.add(status);
+
+//        creating second panel
+        a1 = new JPanel();
+        a1.setBounds(5, 75, 440, 570);
+        f.add(a1);
+
+        text = new JTextField();
+        text.setBounds(5, 655, 310, 40);
+        text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(text);
+
+        send = new JButton("Send");
+        send.setBounds(320, 655, 123, 40);
+        send.setBackground(new Color(7, 94, 84));
+        send.setForeground(Color.WHITE);
+        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        send.addActionListener(this);
+        f.add(send);
+
+        f.setSize(450, 700);
+        f.setLocation(200, 50);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
+
+        f.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent ae){
+        try {
+            if (ae.getSource() == send) {
+                String out = text.getText();
+//            getting label via function to format the message
+                JPanel p2 = formatLabel(out);
+//            System.out.println(out);
+//            using border layout to put layouts on top, bottom, left, right, centre
+                a1.setLayout(new BorderLayout());
+//      setting it to right side
+                JPanel right = new JPanel(new BorderLayout());
+//            its argument doesn't accept string only panel so setting text into layout and that into panel
+                right.add(p2, BorderLayout.LINE_END);
+//            now putting that into box and to right hand side
+                vertical.add(right);
+//            space between two vertical struct
+                vertical.add(Box.createVerticalStrut(15));
+//            now adding this into a1 panel
+                a1.add(vertical, BorderLayout.PAGE_START);
+//            now reloading the frame to show the message while the program is running
+
+//                sending out data to client
+                dout.writeUTF(out);
+
+                text.setText("");
+
+                f.repaint();
+                f.invalidate();
+                f.validate();
+
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    public static JPanel formatLabel(String out){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel output = new JLabel("<html><p style=\"width: 150px\">"+ out + "</p></html>");
+        output.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        output.setBackground(new Color(37, 211, 102));
+        output.setOpaque(true);
+//        creating padding, filling border with emptiness for message
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        panel.add(output);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        JLabel time = new JLabel();
+        time.setText(sdf.format(cal.getTime()));
+
+        panel.add(time);
+
+        return panel;
+    }
+
+    public static void main(String[] args) {
+        new Server();
+
+        try{
+//            making server
+//            passing port number
+            ServerSocket skt = new ServerSocket(6001);
+//            accepting all messages so using while loop
+            while(true){
+                Socket s = skt.accept();
+//                making class to receive data
+                DataInputStream din = new DataInputStream(s.getInputStream());
+//                sending messages out
+                dout = new DataOutputStream(s.getOutputStream());
+//                now using protocols to send and receive
+                while(true){
+                    String msg = din.readUTF(); // protocol
+//                    displaying the message
+                    JPanel panel = formatLabel(msg);
+
+//                    to show the message to left side
+                    JPanel left = new JPanel(new BorderLayout());
+                    left.add(panel, BorderLayout.LINE_START);
+//                    calling non-static functions like vertical, validate from static function main so showing error
+//                    so converting the nature to static
+                    vertical.add(left);
+//                    so we have to create object of the JFrame class so we can't extends this directly
+                    f.validate();
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
